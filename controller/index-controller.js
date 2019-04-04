82fe470a8e019af371f079c28d0dc3bb935df46e
loribao <lorival.sanjinez@hotmail.com>
2019-04-03 23:44:16 -0300

    removido

--- controller/index-controller.js
+++
@@ -1,6 +0,0 @@
-exports.get = (req, res) => {
-    var teste = require('../model/index-model');
-
-    res.status(200).render('main.ejs', { body: teste(req, res, "<p>oitoPilha1R$<p>"), rodape: "<hr><p>novePilha1R$" }
-    );
-};
--- model/index-model.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, args) => {
-    /**
-     * acesso ao banco de dados  
-     */
-
-     
-    return args;
-};
